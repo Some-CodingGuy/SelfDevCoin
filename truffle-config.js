df8120a5e9258319678939ed7783380abc781306
narcis_formagiu <narcis.formagiu@elision.eu>
2022-05-13 18:38:44 +0200

    Config file containing the connection config for the network, now it being set up for ganache locally

--- truffle-config.js
+++ truffle-config.js
@@ -1,25 +1,4 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- * 
- * https://trufflesuite.com/docs/truffle/reference/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
@@ -35,6 +14,12 @@ module.exports = {
    */
 
   networks: {
+	  development: {
+		  host:"127.0.0.1",
+		  port:"7545",
+		  network_id: "*"
+	  }
+	  
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache, geth, or parity) in a separate terminal
