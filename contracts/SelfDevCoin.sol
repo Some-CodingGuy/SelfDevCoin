47ff990aa62e851e9bc4fa290c33e6d88b91d164
narcis_formagiu <narcis.formagiu@elision.eu>
2022-05-13 18:35:25 +0200

    Started smart contract for the self dev coin

---
+++ contracts/SelfDevCoin.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.13;
+
+
+contract SelfDevCoin {
+
+	// constructor
+	// set total supply
+	// read total supply
+
+	uint256 public totalSupply;
+
+	constructor() public {
+		totalSupply = 1000000;
+	}
+}
